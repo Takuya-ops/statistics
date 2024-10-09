780843560f8666d65af9d776fe449a4f7d1aab97
Takuya-ops <hora2112.ipu@gmail.com>
2024-10-09 20:42:14 +0900

    add graph

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -1,9 +1,13 @@
+# モジュールのインポート
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+# 円周率を求める関数
 def estimate_pi(n):
+    # 円の中にある点の数
     points_inside_circle = 0
+    # 打った点の数
     total_points = n
 
     x_inside = []
@@ -14,10 +18,12 @@ def estimate_pi(n):
     for _ in range(total_points):
         x = np.random.uniform(-1, 1)
         y = np.random.uniform(-1, 1)
+        # 円の面積内なら
         if x**2 + y**2 <= 1:
             points_inside_circle += 1
             x_inside.append(x)
             y_inside.append(y)
+        # 円の外側の場合
         else:
             x_outside.append(x)
             y_outside.append(y)
@@ -27,7 +33,7 @@ def estimate_pi(n):
 
 
 # パラメータ設定
-n_points = 10000  # 点の数を減らして処理を軽くする
+n_points = 10000  # 点の数を減らして処理を軽くする（点の数が多いほど正式な円周率に近づいていく）
 
 # πの推定とデータ点の取得
 estimated_pi, x_inside, y_inside, x_outside, y_outside = estimate_pi(n_points)
